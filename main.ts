4108a20d41eb55e80b084fa1ce85d3513f0bac9e
lovederh <lovederh@126.com>
2018-05-14 17:06:40 +0800

    add eyes

--- main.ts
+++ main.ts
@@ -1,14 +1,28 @@
 //% color=195 weight=100 icon="\uf1ec" block="Math Blocks"
 namespace hyedu{
     //% blockId=hyedu_show_smile_face
-    //% block="%eye1|%eye2"
-    export function showSmileFace(eye1: number, eye2: number):void { 
-        basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . * . .
-        # . . . #
-        . # # # .
-        `);
+    //% block="%eye1"
+    export function showSmileFace(eye1: number): void { 
+        if (eye1 === 1) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . * . .
+                # . . . #
+                . # # # .
+                `);
+        } else { 
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `);
+        }
+    }
+    //% blockId=hyedu_get_eyes block="eyes %x"
+    export function getEyes(x: number): number { 
+        return x;
     }
 }
