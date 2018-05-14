b05e563e2e31a6a4ca6b2c77801c3d0debc397a9
lovederh <lovederh@126.com>
2018-05-14 16:53:16 +0800

    icon

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color=195 weight=100 icon="\f1ec" block="Math Blocks"
+//% color=195 weight=100 icon="\uf1ec" block="Math Blocks"
 namespace hyedu{
     //% blockId=hyedu_show_smile_face
     //% block="%eye1|%eye2"
