8666b466a832ee78a13e946bc199acac47d5b5ab
lovederh <lovederh@126.com>
2018-05-14 15:18:25 +0800

    more parameters

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color=195 weight=100 icon="\f1ec" block="Math Blocks"
 namespace hyedu{
     //% blockId=hyedu_show_smile_face
-    //% block="%eye1 %eye2"
+    //% block="%eye1|%eye2"
     export function showSmileFace(eye1: number, eye2: number):void { 
         basic.showLeds(`
         . . . . .
